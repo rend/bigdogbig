6baea3a9696cc4656346417c320b350ae45b231d
rendist <matthews.will@gmail.com>
2020-07-29 21:35:59 +0100

    Added localstorage + reset button + blog 2

--- 1/increment_button.js
+++ 1/increment_button.js
@@ -1,21 +1,32 @@
 'use strict';
 
-const e = React.createElement;
-
-class IncrementButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 0 };
-    }
-
-    render(){
-        return e(
-            'button',
-            {className: "btnPrimary", onClick: () => this.setState({count: this.state.count+1})},
-            this.state.count
-        );
-    }
+function useStickyState(defaultValue, key) {
+    const [value, setValue] = React.useState(() => {
+        const stickyValue = window.localStorage.getItem(key);
+        return stickyValue !== null
+        ? JSON.parse(stickyValue)
+        : defaultValue;
+    });
+    React.useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
 }
 
+function App() {
+    const [count, setCount] = useStickyState(0, "count");
+  
+    return (
+        <div>
+            <button className="btnPrimary" onClick={() => setCount(count + 1)} >
+                {count}
+            </button>
+            <button className="btnPrimary" onClick={() => setCount(0)} >
+                Reset
+            </button>
+        </div> 
+    );
+  }
+  
 const domContainer = document.querySelector('#increment_button_container');
-ReactDOM.render(e(IncrementButton), domContainer);
+ReactDOM.render(<App />, domContainer);
